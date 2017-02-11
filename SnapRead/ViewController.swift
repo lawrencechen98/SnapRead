2d8390a626879c85aa2c85ab7fb6c005a10badb0
lawrencechen98 <lawrencechen98@gmail.com>
2017-02-11 15:10:49 -0800

    Initial commit, with Tesseract Pod and gitignore

---
+++ SnapRead/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapRead
+//
+//  Created by Lawrence Chen on 2/11/17.
+//  Copyright © 2017 Lawrence Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
